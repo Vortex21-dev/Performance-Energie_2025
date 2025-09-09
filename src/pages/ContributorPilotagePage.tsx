fa4c56e5a8db0769760ad389a62316205b2f7c1a
Vortex21-dev <154286034+Vortex21-dev@users.noreply.github.com>
2025-09-09 01:49:00 +0000

    Updated ContributorPilotagePage.tsx

--- src/pages/ContributorPilotagePage.tsx
+++ src/pages/ContributorPilotagePage.tsx
@@ -1859,7 +1859,7 @@ const ContributorPilotagePage = () => {
                       Processus
                     </th>
                     <th scope="col" className="w-12 px-6 py-3"></th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-64">
                       Indicateur
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
